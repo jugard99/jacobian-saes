cc1c80407aadde83cb6ab0bf24f85025e8f8ca3f
jugard99 <jugard@ucsc.edu>
2025-08-05 20:59:39 -0700

    feature acts shape 2 shape

--- jacobian_saes/training/training_sae_pair.py
+++ jacobian_saes/training/training_sae_pair.py
@@ -485,6 +485,7 @@ class TrainingSAEPair(SAEPair):
                 self.apply_sae(z, True, current_l1_coefficient)
             )
             print(f"Feature acts 2 shape: {feature_acts2.shape}")
+            print(f"Feature acts 1 shape: {feature_acts.shape}")
             jacobian = self.compute_head_jacobian(*ctx,topk_indices,topk_indices2)
             # Calculate the Jacobian
             # Change to new jacobian calculation (based on tokens because yeah)
